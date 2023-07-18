3dcf75d9d2bdc248d2181d5e3815a9b19c62c2ef
michelepiccinno <michele.piccinno@gmail.com>
2023-07-18 20:27:43 +0200

    add like counter

--- js/main.js
+++ js/main.js
@@ -57,47 +57,70 @@ const posts = [
     }
 ];
 
-//FUNCTION PER CICLARE
+//FUNZIONE CHE CICLA L'ARRAY DI OGGETTI
 
-const postCard = document.createElement("div");
-postCard.classList.add("post");
+posts.forEach(singleObject => {
+    console.log(singleObject);
+    const id = singleObject.id;
+    const content = singleObject.content;
+    const media = singleObject.media;
+    const authorName = singleObject.author.name;
+    const authorImage = singleObject.author.image;
+    const likes = singleObject.likes;
+    const created = singleObject.created;
 
-postCard.innerHTML = `<div class="post">
+    //CREO IL IL DIV NEL QUALE INIETTARE TUTTO IL CODICE, ASSEGNO LA CLASSE E INIETTO L'HTML
+    const postCard = document.createElement("div");
+    postCard.classList.add("post");
+    postCard.innerHTML = `<div class="post">
 <div class="post__header">
     <div class="post-meta">                    
         <div class="post-meta__icon">
-            <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+            <img class="profile-pic" src="${authorImage}" alt="Phil Mangione">                    
         </div>
         <div class="post-meta__data">
-            <div class="post-meta__author">Phil Mangione</div>
-            <div class="post-meta__time">4 mesi fa</div>
+            <div class="post-meta__author">${authorName}</div>
+            <div class="post-meta__time">${created}</div>
         </div>                    
     </div>
 </div>
-<div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+<div class="post__text">${content}</div>
 <div class="post__image">
-    <img src="https://unsplash.it/600/300?image=171" alt="">
+    <img src="${media}" alt="">
 </div>
 <div class="post__footer">
     <div class="likes js-likes">
         <div class="likes__cta">
-            <a class="like-button  js-like-button" href="#" data-postid="1">
+            <a class="like-button  js-like-button" data-postid="${id}">
                 <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                 <span class="like-button__label">Mi Piace</span>
             </a>
         </div>
         <div class="likes__counter">
-            Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+            Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
         </div>
     </div> 
 </div>            
 </div>`
 
-postCard.querySelector(".like-button").addEventListener("click", function () {
-    postCard.querySelector(".js-likes-counter").textContent = 'nuovo_valore';
-
+    //RICHIAMO IL CONTAINER GENERALE E "APPENDO" TUTTO IL CONTENUTO
+    const generalContainer = document.getElementById("container");
+    generalContainer.appendChild(postCard);
 })
 
-const generalContainer = document.getElementById("container");
-generalContainer.appendChild(postCard);
 
+posts.forEach((element, i) => {
+   const arrayPostId = []; 
+    const btnLike = document.querySelectorAll(".js-like-button");
+    const countLike = document.querySelectorAll("#like-counter-1");
+    btnLike[i].addEventListener("click", function () {
+        const toggleBtn = btnLike[i].classList.toggle("like-button--liked");
+        if (toggleBtn === true) {
+            countLike[i].innerHTML = posts[i].likes + 1;
+        } else {
+            countLike[i].innerHTML = posts[i].likes - 1;
+        }
+        arrayPostId.push(posts[i].id)
+        console.log(arrayPostId);
+    })
+})
